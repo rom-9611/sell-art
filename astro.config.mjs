ccb0fb9937a6572112236be51532fd0ed2b676c8
rom-9611 <rom785614@gmail.com>
2026-09-04 23:19:35 +0530

    Fix navigation paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'; // or '@astrojs/tailwind', depending on your setup
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://rom-9611.github.io',
-  base: '/sell-art',
+  site: "https://rom-9611.github.io",
+  base: "/sell-art",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
